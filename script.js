3529ecee168f13d371dd3fad8f3a51be5692b7ba
md-bappi <mdabuanasbappy555@gmail.com>
2023-11-05 19:37:07 +0600

    tab component done

--- script.js
+++ script.js
@@ -194,3 +194,26 @@ dotContainer.addEventListener("click", function (e) {
 // button handles
 btnLeft.addEventListener("click", previousSlide);
 btnRight.addEventListener("click", nextSlide);
+
+// keyboard
+document.addEventListener("keydown", (e) => {
+  e.key === "ArrowLeft" && previousSlide();
+  e.key === "ArrowRight" && nextSlide();
+});
+
+// tabbed components
+tabsContainer.addEventListener("click", function (e) {
+  const btn = e.target.closest(".operations__tab");
+
+  if (!btn) return;
+
+  tabs.forEach((tab) => tab.classList.remove("operations__tab--active"));
+  tabsContent.forEach((content) =>
+    content.classList.remove("operations__content--active")
+  );
+
+  btn.classList.add("operations__tab--active");
+  document
+    .querySelector(`.operations__content--${btn.dataset.tab}`)
+    .classList.add("operations__content--active");
+});
