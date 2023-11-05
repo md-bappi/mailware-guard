4669ad211efa4073f09fda95b03fc33fa59a6220
md-bappi <mdabuanasbappy555@gmail.com>
2023-11-05 19:13:54 +0600

    slider done

--- script.js
+++ script.js
@@ -113,3 +113,84 @@ toggleBtn.addEventListener("click", function () {
     document.querySelector("html").style.overflow = "hidden";
   }
 });
+
+// lazy img loading
+const imgLoading = function (entries) {
+  const entry = entries[0];
+  if (!entry.isIntersecting) return;
+  entry.target.src = entry.target.dataset.src;
+
+  entry.target.addEventListener("load", function () {
+    entry.target.classList.remove("lazy-img");
+  });
+};
+const imgObserver = new IntersectionObserver(imgLoading, {
+  root: null, // viewport
+  threshold: 0,
+  rootMargin: `252px`,
+});
+
+imgTargets.forEach((img) => {
+  imgObserver.observe(img);
+});
+
+// Slider
+let currentSlide = 0;
+const maxSlide = slides.length - 1;
+
+// DOts
+function creatingDots() {
+  slides.forEach((_, i) => {
+    const dot = `<button class="dots__dot" data-slide="${i}"></button>`;
+    dotContainer.insertAdjacentHTML("beforeend", dot);
+  });
+}
+
+creatingDots();
+
+// Activate dots
+function activateDots(slide) {
+  document
+    .querySelectorAll(".dots__dot")
+    .forEach((dot) => dot.classList.remove("dots__dot--active"));
+
+  document
+    .querySelector(`.dots__dot[data-slide="${slide}"]`)
+    .classList.add("dots__dot--active");
+}
+
+activateDots(0);
+
+function updateSlide(cs) {
+  slides.forEach(
+    (sl, i) => (sl.style.transform = `translateX(${100 * (i - cs)}%)`)
+  );
+}
+
+updateSlide(0);
+
+function previousSlide() {
+  if (currentSlide === 0) currentSlide = maxSlide;
+  else currentSlide--;
+  updateSlide(currentSlide);
+  activateDots(currentSlide);
+}
+
+function nextSlide() {
+  if (currentSlide === maxSlide) currentSlide = 0;
+  else currentSlide++;
+  updateSlide(currentSlide);
+  activateDots(currentSlide);
+}
+
+// dots handler
+dotContainer.addEventListener("click", function (e) {
+  if (e.target.classList.contains("dots__dot")) {
+    activateDots(e.target.dataset.slide);
+    updateSlide(e.target.dataset.slide);
+  }
+});
+
+// button handles
+btnLeft.addEventListener("click", previousSlide);
+btnRight.addEventListener("click", nextSlide);
