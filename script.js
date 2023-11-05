d6359444be24f83f9285f0cc617b0fd854fcc61a
md-bappi <mdabuanasbappy555@gmail.com>
2023-11-05 15:59:29 +0600

    toggle btn done

--- script.js
+++ script.js
@@ -6,33 +6,110 @@
 
 const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
-const btnsOpenModal = document.querySelectorAll(".btn-show-modal");
-const btnCloseModal = document.querySelector(".btn-close-modal");
+const btnsOpenModal = document.querySelectorAll(".btn--show-modal");
+const btnCloseModal = document.querySelector(".btn--close-modal");
 const nav = document.querySelector(".nav");
-const navLinks = document.querySelector(".nav-links");
+const navLinks = document.querySelector(".nav__links");
 const header = document.querySelector(".header");
-const toggleBtn = document.querySelector(".nav-toggle");
-const btnScrollTo = document.querySelector(".btn-scroll-to");
+const toggleBtn = document.querySelector(".nav__toggle");
+const btnScrollTo = document.querySelector(".btn--scroll-to");
 const allSections = document.querySelectorAll(".section");
-const section1 = document.querySelector("#section-1");
-const tabsContainer = document.querySelector(".operations-tab-container");
-const tabs = document.querySelectorAll(".operations-tab");
-const tabsContent = document.querySelectorAll(".operations-content");
+const section1 = document.querySelector("#section--1");
+const tabsContainer = document.querySelector(".operations__tab-container");
+const tabs = document.querySelectorAll(".operations__tab");
+const tabsContent = document.querySelectorAll(".operations__content");
 const cookieBody = document.querySelector(".cookie");
-const cookieCloseBtn = document.querySelector(".cookie-close");
+const cookieCloseBtn = document.querySelector(".cookie__close");
 const imgTargets = document.querySelectorAll("img[data-src]");
 const slides = document.querySelectorAll(".slide");
 const slider = document.querySelector(".slider");
-const btnLeft = document.querySelector(".slider-btn-left");
-const btnRight = document.querySelector(".slider-btn-right");
+const btnLeft = document.querySelector(".slider__btn--left");
+const btnRight = document.querySelector(".slider__btn--right");
 const dotContainer = document.querySelector(".dots");
-///////////////////////////////////
-// cookie
-///////////////////////////////////
+
+// cookies
 cookieCloseBtn.addEventListener("click", function () {
   cookieBody.classList.add("hidden");
-  cookieBody.style.bottom = "-9rem";
+  cookieBody.style.bottom = `-12rem`;
 });
-///////////////////////////////////
 
-///////////////////////////////////
+// nav height
+
+const navHeight = nav.getBoundingClientRect().height;
+
+const sticky = function (entries, observe) {
+  const entry = entries[0];
+
+  if (!entry.isIntersecting) nav.classList.add("sticky");
+  else nav.classList.remove("sticky");
+};
+
+const headerObs = new IntersectionObserver(sticky, {
+  root: null, // viewport
+  threshold: 0,
+  rootMargin: `-${navHeight}px`,
+});
+headerObs.observe(header);
+
+// reveal section
+function revealSection(entries) {
+  // const entry = entries[0];
+  const [entry] = entries;
+  if (!entry.isIntersecting) return;
+  else entry.target.classList.remove("section--hidden");
+}
+
+const sectionObs = new IntersectionObserver(revealSection, {
+  root: null, // viewport
+  threshold: 0,
+});
+
+allSections.forEach((section) => {
+  sectionObs.observe(section);
+  section.classList.add("section--hidden");
+});
+
+// modal window
+const modalwindow = function (e) {
+  e.preventDefault();
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+  document.querySelector("html").style.overflow = "hidden";
+};
+
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+  document.querySelector("html").style.overflow = "visible ";
+};
+btnsOpenModal.forEach((btn) => btn.addEventListener("click", modalwindow));
+btnCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+    closeModal();
+  }
+});
+
+// scroll behavior
+
+navLinks.addEventListener("click", function (e) {
+  e.preventDefault();
+  if (e.target.classList.contains("nav__link")) {
+    const attr = e.target.getAttribute("href");
+    console.log(attr);
+    document.querySelector(attr).scrollIntoView({ behavior: "smooth" });
+  }
+});
+
+// toggle btn
+toggleBtn.addEventListener("click", function () {
+  if (navLinks.classList.contains("nav__open")) {
+    navLinks.classList.remove("nav__open");
+    document.querySelector("html").style.overflow = "visible";
+  } else {
+    navLinks.classList.add("nav__open");
+    document.querySelector("html").style.overflow = "hidden";
+  }
+});
